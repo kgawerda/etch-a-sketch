a2b402d59ceebf9dd3314f7e34b294e73d668086
Konrad <konradgawerda@gmail.com>
2022-09-14 19:22:03 +0200

    fix grid

--- javascript.js
+++ javascript.js
@@ -1,15 +1,11 @@
-const grid=document.getElementById('grid');
-function grid_init(n){
-    for(let i=0; i<n; i++){
-        const vertical=document.createElement("div");
-        vertical.classList.add('grid-vertical');
-        let row=grid.appendChild(vertical);
-        for(let j=0; j<n; j++){
-            const horizontal=document.createElement("div");
-            horizontal.classList.add('grid-horizontal');
-            row.appendChild(horizontal);
-        }
+const grid = document.getElementById('grid');
+function grid_init(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement("div");
+        gridElement.classList.add('grid-element');
+        grid.appendChild(gridElement);
     }
-
 }
 grid_init(16);
