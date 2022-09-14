bc4ef42a1907e957e0a0f9822e631d0ded7186d6
Konrad <konradgawerda@gmail.com>
2022-09-14 19:42:52 +0200

    Add color change

--- javascript.js
+++ javascript.js
@@ -1,11 +1,26 @@
 const grid = document.getElementById('grid');
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+let size=16;
 function grid_init(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
     for (let i = 0; i < size * size; i++) {
         const gridElement = document.createElement("div");
         gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mouseover', changeColor)
+        gridElement.addEventListener('mousedown', changeColor)
         grid.appendChild(gridElement);
     }
 }
-grid_init(16);
+
+grid_init(size);
+
+function changeColor(e){
+    if (e.type === 'mouseover' && !mouseDown) return
+    e.target.style.backgroundColor='black';
+}
+
