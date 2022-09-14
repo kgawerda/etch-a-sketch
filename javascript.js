377882662a46f248d3166ce9b6deba4614231091
Konrad <konradgawerda@gmail.com>
2022-09-14 20:21:37 +0200

    add clear and size buttons

--- javascript.js
+++ javascript.js
@@ -1,10 +1,15 @@
 const grid = document.getElementById('grid');
+const clearButton = document.getElementById('clear');
+const sizeButton = document.getElementById('grid-size');
+
+clearButton.onclick = () => clearGrid();
+sizeButton.onclick = () => changeSize();
 
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
-let size=16;
+
 function grid_init(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
@@ -17,10 +22,39 @@ function grid_init(size) {
     }
 }
 
-grid_init(size);
 
-function changeColor(e){
+
+function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown) return
-    e.target.style.backgroundColor='black';
+    e.target.style.backgroundColor = 'black';
 }
 
+function clearGrid() {
+    deleteGrid();
+    grid_init(currentSize);
+}
+
+function deleteGrid() {
+    let child = grid.lastElementChild;
+    while (child) {
+        grid.removeChild(child);
+        child = grid.lastElementChild;
+    }
+}
+
+function changeSize() {
+    let size = Number(prompt("Please choose grid size"));
+    while (isNaN(size)) {
+        size = Number(prompt("Not a number, try again"));
+    }
+    while (size >= 100) {
+        size = Number(prompt("Please enter a number lower than 100"));
+    }
+    deleteGrid();
+    grid_init(size);
+    currentSize = size;
+}
+
+const defaultSize = 16;
+let currentSize = defaultSize;
+grid_init(defaultSize);
